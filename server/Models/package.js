e7afb4fbcc444f7c8fbf74a7367391aff946a90e
Abbas <abbasalizada252@gmail.com>
2025-12-06 16:34:12 +0430

    model added packList

--- server/Models/package.js
+++ server/Models/package.js
@@ -17,13 +17,35 @@ const Package = sequelize.define(
     totalWeight: DataTypes.FLOAT,
     piece: DataTypes.INTEGER,
     value: DataTypes.FLOAT,
-    transitWay:DataTypes.STRING,
+    transitWay: DataTypes.STRING,
     OPerKgCash: DataTypes.FLOAT,
     OTotalCash: DataTypes.FLOAT,
     perKgCash: DataTypes.FLOAT,
     totalCash: DataTypes.FLOAT,
     remain: DataTypes.FLOAT,
     received: DataTypes.FLOAT,
+
+    // --- FIXED packList ---
+    packList: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: [],
+      comment: "List of packages: each item contains {weight, description, qty}",
+      get() {
+        const raw = this.getDataValue("packList");
+        return raw ? raw : [];
+      },
+      set(value) {
+        // Ensure it's always an array
+        if (Array.isArray(value)) {
+          this.setDataValue("packList", value);
+        } else if (typeof value === "object") {
+          this.setDataValue("packList", [value]);
+        } else {
+          this.setDataValue("packList", []);
+        }
+      },
+    },
   },
   {
     tableName: "packages",
@@ -43,4 +65,5 @@ Package.belongsTo(Customer, {
   foreignKey: "receiver",
   targetKey: "id",
 });
-export default Package
+
+export default Package;
